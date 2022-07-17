058548a1729e5043c097136764127611848bd4d5
coders-sohan <coders.sohan.0216@gmail.com>
2022-07-17 10:11:37 +0600

    header section updated and navigation menu also updated

--- next-shop/components/Layout.js
+++ next-shop/components/Layout.js
@@ -1,10 +1,12 @@
-import Head from "next/head";
 import React from "react";
+import Head from "next/head";
+import NextLink from "next/link";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import useStyles from "../utils/styles";
+import { Link } from "@mui/material";
 
 export default function Layout({ children }) {
 	const classes = useStyles();
@@ -16,7 +18,20 @@ export default function Layout({ children }) {
 			</Head>
 			<AppBar position="static" className={classes.navbar}>
 				<Toolbar>
-					<Typography variant="h6">Next-Shop</Typography>
+					<NextLink href="/" passHref>
+						<Link style={{ textDecoration: "none" }}>
+							<Typography className={classes.brand}>Next-Shop</Typography>
+						</Link>
+					</NextLink>
+					<div className={classes.grow}></div>
+					<div>
+						<NextLink href="/cart" passHref>
+							<Link style={{ textDecoration: "none" }}>Cart</Link>
+						</NextLink>
+						<NextLink href="/login" passHref>
+							<Link style={{ textDecoration: "none" }}>Login</Link>
+						</NextLink>
+					</div>
 				</Toolbar>
 			</AppBar>
 			<Container className={classes.main}>{children}</Container>
--- next-shop/pages/index.js
+++ next-shop/pages/index.js
@@ -1,4 +1,15 @@
 import Layout from "../components/Layout";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import data from "../utils/data";
+import {
+	CardActionArea,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Typography,
+	Button,
+} from "@mui/material";
 
 export default function Home() {
 	return (
@@ -6,11 +17,34 @@ export default function Home() {
 			<Layout>
 				<div>
 					<h1>Products</h1>
-					<ul>
-						<li>Product 01</li>
-						<li>Product 02</li>
-						<li>Product 03</li>
-					</ul>
+					<Grid container spacing={3}>
+						{data.products.map((product) => (
+							<Grid item md={4} key={product.name}>
+								<Card>
+									<CardActionArea>
+										<CardMedia
+											component="img"
+											image={product.image}
+											title={product.name}
+										/>
+										<CardContent>
+											<Typography variant="h6" color="initial">
+												{product.name}
+											</Typography>
+										</CardContent>
+									</CardActionArea>
+									<CardActions>
+										<Typography variant="p" color="initial">
+											${product.price}
+										</Typography>
+										<Button size="small" color="primary">
+											Add to Cart
+										</Button>
+									</CardActions>
+								</Card>
+							</Grid>
+						))}
+					</Grid>
 				</div>
 			</Layout>
 		</>
